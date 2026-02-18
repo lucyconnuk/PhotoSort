171a6142e98de4e5df3d73c87dc4baa56fbfc7d0
Lucy Connelly <lucy@lucy-connelly.co.uk>
2026-02-18 14:58:26 +0000

    Renaming for clarity

--- photosort.py
+++ photosort.py
@@ -1,25 +1,25 @@
 from pathlib import Path
 
-def check_directory_exists( directory ):
-    directory_exists = False;
+def path_is_valid( path: Path ):
+    path_is_valid = False;
 
-    if directory.exists():
-        if directory.is_dir():
-            print( f"{directory} is a directory" )
-            directory_exists = False;
-        elif directory.is_file():
-            print( f"{directory} is a file" )
+    if path.exists():
+        if path.is_dir():
+            print( f"{path} is a directory" )
+            path_is_valid = True;
+        elif path.is_file():
+            print( f"{path} is a file" )
         else:
-            print( f"{directory} exists but is not a directory or a file" )
+            print( f"{path} exists but is not a directory or a file" )
     else:
-        print( f"{directory} does not exist" )
+        print( f"{path} does not exist" )
 
-    return directory_exists
+    return path_is_valid
 
 def main():
-    directory_path = input( "Enter directory path: " ).strip()
-    directory = Path( directory_path )
-    check_directory_exists( directory )
+    path_str = input( "Enter directory path: " ).strip()
+    path = Path( path_str )
+    path_is_valid( path )
 
 if __name__ == "__main__":
     main()
