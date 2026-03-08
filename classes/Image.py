5047c7f7990bfd5b5d569be0ef9d719cd7e5c64d
Lucy Connelly <lucy@lucy-connelly.co.uk>
2026-03-08 13:26:32 +0000

    Remove sidecar_filename for now. Change property names to better match / represent data.

--- classes/Image.py
+++ classes/Image.py
@@ -9,18 +9,24 @@ from typing import Optional
 @dataclass(frozen=True)
 class Image:
     
-    image_filename: Path
-    sidecar_filename: Optional[Path] = None
-    camera: Optional[Camera] = None
-    taken_date: Optional[datetime] = None
+    image_path: Path
+    camera_model: Optional[Camera] = None
+    date_taken: Optional[datetime] = None
     owner: Optional[Owner] = None
-    initial_capture: Optional[ImageCaptureType] = None
+    initial_capture_type: Optional[ImageCaptureType] = None
+    image_expected_path: Optional[Path] = None
+    #sidecar_filename: Optional[Path] = None
 
     def __post_init__(self):
 
-        if self.sidecar_filename and self.sidecar_filename.suffix != ".xmp":
-            raise ValueError("Sidecar file must be an .xmp file")
+        # Set owner from camera_model and date_taken
+
+        # Set initial_capture_type from camera_model
+        if self.camera_model and not self.initial_capture_type:
+            self.initial_capture_type = self.camera_model.image_capture_type
+
+        # Set image_expected_path from date_taken, owner and initial_capture_type
+
+        #if self.sidecar_filename and self.sidecar_filename.suffix != ".xmp":
+        #    raise ValueError("Sidecar file must be an .xmp file")
         
-        if self.camera and not self.initial_capture:
-            # Set image initial capture method to camera image capture type
-            self.initial_capture = self.camera.image_capture_type
