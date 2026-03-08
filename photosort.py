33327ddd0712305728fba226fb16f7a0040a6992
Lucy Connelly <lucy@lucy-connelly.co.uk>
2026-03-08 13:27:08 +0000

    Progress towards where we want to be.

--- photosort.py
+++ photosort.py
@@ -1,9 +1,10 @@
 import argparse
 from classes.Camera import Camera
 from classes.File import File
-from classes.Image import Image
-from classes.ImageCaptureType import ImageCaptureType
+#from classes.ImageCaptureType import ImageCaptureType
 from classes.Owner import Owner
+from classes.PathFormat import PathFormat
+
 from pathlib import Path
 
 # TODO:
@@ -37,35 +38,43 @@ def main():
 
         # Find all the files in the directory.
         files_and_dirs = path.rglob("*")
-        files = File.get_files( files_and_dirs )
+        file_paths = File.get_files( files_and_dirs )
         
         # Find all the image files in the set.
         print( "Images are: ")
-        images = File.get_image_files( files )
-        for image in images:
-            print( image.name )
+        image_paths = File.get_image_files( file_paths )
+        for image_path in image_paths: print( image_path.name )
 
         # Find all the XMP files in the set.
         print( "XMP files are: ")
-        xmp_files = File.get_xmp_files( files )
-        for xmp_file in xmp_files:
-            print( xmp_file.name )
+        xmp_file_paths = File.get_xmp_files( file_paths )
+        for xmp_file_path in xmp_file_paths: print( xmp_file_path.name )
 
-    else:
-        my_image = Image( "test.jpg", initial_capture=ImageCaptureType.Film )
-        print( my_image )
+    cameras = Camera.get_all( r'./data/cameras.csv' )
+    # for camera in cameras: print( camera )
 
-    camera_data_file = Path( r'./data/cameras.csv' )
-    cameras_df = Camera.load_all( camera_data_file )
-    cameras = Camera.dataframe_to_list( cameras_df )
-    for camera in cameras:
-        print( camera )
+    owners = Owner.get_all( r'./data/owners.csv' )
+    # for owner in owners: print( owner )
 
-    owner_data_file = Path( r'./data/owners.csv' )
-    owners_df = Owner.load_all( owner_data_file )
-    owners = Owner.dataframe_to_list( owners_df )
-    for owner in owners:
-        print( owner )
+    path_formats = PathFormat.get_all( r'./data/path_formats.csv' )
+    # for path_format in path_formats: print( path_format )
+
+    # matching_pfs = [ pf
+    #     for pf in path_formats 
+    #     if pf.owner_name == "Mary"
+    #     and pf.image_capture_type == ImageCaptureType.Film.value
+    # ]
+    # print("***************************************")
+    # for matching_pf in matching_pfs: print( matching_pf )
+
+
+    # image = new Image(...);
+    # camera = image.get_camera( cameras )
+    # owner = camera.Owner
+    # image.Owner = camera.Owner
+    # image.Initialcapture = camera.InitialCapture
+    # image.actual_path # where file is now
+    # image.expected_path # where file should be - depends on image_capture_type, owner and date_taken
 
 if __name__ == "__main__":
     main()
